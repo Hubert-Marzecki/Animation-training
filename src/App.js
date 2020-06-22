e10d68e94b001c371627b72a429ddee8f7b14daf
Hubert Marzecki <hubert.marzecki@gmail.com>
2020-06-22 19:17:19 +0200

    nav added

--- src/App.js
+++ src/App.js
@@ -1,66 +1,30 @@
-import React, {useEffect, useState} from 'react';
-import './App.css';
-import {useSpring, useTrail, animated} from 'react-spring'
+import React from 'react';
+import './App.scss';
+// import {useSpring, useTrail, animated} from 'react-spring'
+// import { jsx } from '@emotion/core;'
+// import { css, cx } from 'emotion';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
+import {Navigation} from "./components/Navigation";
 
 function App() {
-    const fast = { tension: 1000, friction: 40 }
-    const slow = { mass: 1, tension: 100, friction:10 }
-    const trans = (x, y) => `translate3d(${x}px,${y}px,0) translate3d(-50%,-50%,0)`
-
-    // useTrail multiple springs with a single dataset, one spring follows or trails behind the other
-    const [trail, set] = useTrail(3, () => {
-        return ({xy: [0, 0], config: i => (i === 0 ? fast : slow)});
-    })
-
-    // touble - setCard modify card
-    // useSpring a single spring, moves data from a -> b
-    const [card, setCard] = useSpring(() => {
-        return ({xys: [0, 0, 1], config: {mass: 1, tension: 100, friction: 40}});
-    })
-
-
-    // calc is value of proporion on image - x- x , y-y, s- scale img
-    const calc = (x, y) => [(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1.1]
-    // style confugurate position of element
-    const transPos = (x, y, s) => `perspective(500px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
-
-
     return (
+        <Router>
       <div
-      className="App"
-     >
-          <>
-              <svg style={{ position: 'absolute', width: 0, height: 0 }}>
-                  {/* filter elements - add styles to elements */}
-                  <filter id="goo">
-                      <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="30" />
-                      <feColorMatrix in="blur"  values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 10 -7" />
-                  </filter>
-              </svg>
-              {/* add elements based on trail, and change position via "set"*/}
-              {/* TOTO check what intepolate is */}
-              <div className="hooks-main" onMouseMove={e => set({ xy: [e.clientX, e.clientY] })}  >
-                  {trail.map((props, index) => (
-                      <animated.div key={index} style={{ transform: props.xy.interpolate(trans) }} />
-                  ))}
-              </div>
+      className="App">
+    <Navigation
 
-
-          </>
-          <div className="card__container">
-              <animated.div className="card"
-                   {/* add new property based on mouse position*/}
-                   onMouseMove={({ clientX: x, clientY: y }) => setCard({ xys: calc(x, y) })}
-                   {/* reset info about projects */}
-                   onMouseLeave={() => setCard({ xys: [0, 0, 1] })}
-                   {/* TOTO check what intepolate is */}
-                   style={{ transform: card.xys.interpolate(transPos) }}
-                  >
-                  <img src={"https://i.picsum.photos/id/1002/4312/2868.jpg?hmac=5LlLE-NY9oMnmIQp7ms6IfdvSUQOzP_O3DPMWmyNxwo"} />
-                  <h1> CAPTION </h1>
-                  </animated.div>
-          </div>
+    />
+    <Switch>
+        <Route path="/about"> ABOUT </Route>
+        <Route path="/menu"> MENU </Route>
+        <Route path="/"> HOME </Route>
+    </Switch>
       </div>
+        </Router>
   )
 }
 // img to use
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './nav.scss';
+import {
+    Link
+} from "react-router-dom";
+import {useSpring,  animated} from 'react-spring'
+
+
+
+const calc = (x, y) => [-(y - window.innerHeight / 4) / 20, (x - window.innerWidth / 4) / 20, 1.1]
+const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
+
+export function Navigation () {
+    const [props, set] = useSpring(() =>
+        ({ xys: [0, 0, 1], config: { mass: 1, tension: 100, friction: 10 } }))
+
+    return (
+       <ul className="nav">
+           <li className="nav__link"><Link to="/about"> About </Link> </li>
+           <li className="nav__link"
+             >
+               <Link to="/" >
+               <animated.img className="nav__link nav__link--img" src={"https://logopond.com/logos/e6b478cef54b8fb8acfd1b4dee22f543.png"}
+                    onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+                    onMouseLeave={() => set({ xys: [0, 0, 1] })}
+                    style={{ transform: props.xys.interpolate(trans) }}
+                />
+
+           </Link> </li>
+           <li className="nav__link">       <Link to="/menu">Menu</Link> </li>
+       </ul>
+
+    )
+}
