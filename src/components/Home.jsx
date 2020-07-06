e49ed9b2a874a2f7ba3c351263e2b648ff1617b4
Hubert Marzecki <hubert.marzecki@gmail.com>
2020-07-06 14:09:22 +0200

    new layout

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,27 +5,36 @@ import Sushi2 from '../assets/sushi(1).svg'
 import Sushi3 from '../assets/sushi(2).svg'
 import Sushi4 from '../assets/sushi(3).svg'
 import Sushi5 from '../assets/sushi(4).svg'
-import {useSpring, useTrail, animated as a} from 'react-spring'
+import edo from '../assets/edo.jpg';
+import edoTwo from '../assets/sushi-menu.jpg';
+import motoOne from '../assets/moto1.png'
+
+import {useSpring, useTrail, animated as a} from 'react-spring';
+import useMeasure from 'react-use-measure'
 
 import { css, cx } from 'emotion';
 import { useState } from "react";
 const dur = 7000;
 
+
 export function Home() {
     const bg = "https://cdn.statically.io/img/free4kwallpapers.com/uploads/originals/2019/10/29/city-of-japan-wallpaper.jpg"
     const mainFont = `'Krona One', sans-serif`;
-    const [flipped, set] = useState({
-        one: true,
-        two: true 
-    })
 
-    // zparametryzować
+    // used with first menu item
+    const [animationTrigger, setAnimationTrigger] = useState({
+        menuItem: true,
+        contactItem: false,
+        orderItem: false,
+    })
     const { transform, opacity } = useSpring({
-      opacity: flipped.one ? 1 : 0,
-      transform: `perspective(600px) rotateX(${flipped.one ? 180 : 0}deg)`,
+      opacity: animationTrigger.menuItem ? 1 : 0,
+      transform: `perspective(600px) rotateX(${animationTrigger.menuItem ? 180 : 0}deg)`,
       config: { mass: 5, tension: 500, friction: 80 }
     })
- 
+
+
+    //  styles 
     const linkHome = css`
     margin: 1em; 
     font-size: 40px ; 
@@ -33,31 +42,69 @@ export function Home() {
     font-family: ${mainFont};
     cursor: pointer; 
     text-decoration: overline;
-    }
-}
-
      `;
+
+     const animatedImg=css`
+     background-size: cover;
+     background-attachment: fixed;
+     position: fixed;
+     width: 400px;
+     height: 500px;
+     cursor: pointer;
+     `
+    
+
 
 return (
 <div>
-    {/* img  */}
      <>
+         {/* IMG MENU */}
       <a.div className={css` 
       background-image: url("https://pm1.narvii.com/6375/7c1d306ae58f6881abe7b386e70f7c915aaae937_hq.jpg");
-      background-size: cover;
-      background-attachment: fixed;
-      position: absolute;
-      width: 20ch;
-      height: 30ch;
-      left:10%;
+      ${animatedImg}
+      left:20%;
       top:35%;
-      cursor: pointer;
       will-change: transform, opacity;
       `} style={{ opacity: opacity.interpolate(o => 1 - o), transform }} />
-      <a.div class={css``} style={{ opacity, transform: transform.interpolate(t => `${t} rotateX(180deg)`) }} />
+      <a.div className={css``} style={{ opacity, transform: transform.interpolate(t => `${t} rotateX(180deg)`) }} />
     </>
 
+    {/* IMG KONTAKT */}
+    <a.div className={css` 
+      background-image: url(${edo});
+      ${animatedImg}
+      left:40%;
+      ${animationTrigger.contactItem ? `top:50%;` : `top: 100%;`}
+      transition: top 1s;
+      will-change: transform, opacity;
+      `} />
 
+      {/* IMG ZAMÓW */}
+      <a.div className={css` 
+      background-image: url(${edoTwo});
+      ${animatedImg}
+      left:70%;
+      top:30%;
+      ${animationTrigger.orderItem ? `opacity:1;` : `opacity:0;`}
+      transition: opacity 0.5s;
+      will-change: transform, opacity;
+      `} />
+
+    {/*  MOTO ONE */}
+    <a.div className={css` 
+      background-image: url(${motoOne});
+      ${animatedImg}
+      width: 100px;
+      height: 100px;
+      left:50%;
+      top:90%;
+      transition: opacity 0.5s;
+      cursor: none;
+      will-change: transform, opacity;
+      `}  />
+
+
+    {/* APP */}
     <div className={css` 
     height: 100vh;
     color: $text-color;
@@ -67,7 +114,7 @@ return (
     `
     }>
       
-      
+        {/* MENU HOLDER */}
         <div className={
             css`
             position: relative;
@@ -79,24 +126,38 @@ return (
             transform:translate(-50%, -50%)
             `
         }>
-    
+                {/*  ADRRESS  */}
+                <h2 className={css`
+                width: 100%; 
+                text-align:center;   
+                font-family: ${mainFont};
+                font-size: 10px;
+                color:white;
+                `}>123 Fake Street — Seattle, WA — 206-555-7890
+                </h2> 
+        
 
-        <span className={linkHome} onMouseEnter={() => set((s) => {return {...s, one: !s.one}})}onMouseLeave={() => set((s) => {return {...s, one: !s.one}})} >MENU</span>  
-        <span className={linkHome}>KONTAKT</span>    
-        <span className={linkHome}>ZAMÓW</span>              
+            {/*  MENU LINK */}
+            <h2 className={linkHome} 
+                onMouseEnter={() => setAnimationTrigger((s) => {return {...s, menuItem: !s.menuItem}})}
+                onMouseLeave={() => setAnimationTrigger((s) => {return {...s, menuItem: !s.menuItem}})} 
+                >
+                    MENU
+            </h2>  
+
+        {/*  KOTNAKT LINK */}
+        <h2 className={linkHome}  
+        onMouseEnter={() => setAnimationTrigger((s) => {return {...s, contactItem: true}})}
+        onMouseLeave={() => setAnimationTrigger((s) => {return {...s, contactItem: false}})} 
+         >KONTAKT</h2>  
+          {/* ZAMÓW LINK */}
+        <h2 className={linkHome}
+         onMouseEnter={() => setAnimationTrigger((s) => {return {...s, orderItem: true}})}
+         onMouseLeave={() => setAnimationTrigger((s) => {return {...s, orderItem: false}})} 
+        >ZAMÓW</h2>  
+               
         </div>
-        <p className={css`
-        position: absolute; 
-        color:white; 
-        left: 50%; 
-        top:40%;
-        transform:translate(-50%, -50%);
-        font-family: ${mainFont};
-        font-size: 10px;
-`}> 
-
-123 Fake Street — Seattle, WA — 206-555-7890
-</p>
+   
     </div>
     </div >
 )
